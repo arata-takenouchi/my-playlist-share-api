2eb2b09b19d55eb7c123126cc04cccb0c167f003
[arata-takenouchi] <[arata.takenouchi.1230@gmail.com]>
2023-12-29 22:52:20 +0900

    routerのimportを修正

--- api/main.py
+++ api/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-from api.login import login
-from api.playlist import playlist
-from api.user import user
+from api.routers import router
 
 app = FastAPI()
-app.include_router(login.router)
-app.include_router(playlist.router)
-app.include_router(user.router)
+app.include_router(router.router)
 
 @app.get("/hello")
 async def hello():
